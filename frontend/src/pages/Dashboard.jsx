9f8498a2dd094ce44960b277b536061730348643
SakshamDeep0915 <sakshamdeep2608@gmail.com>
2026-08-10 14:09:38 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import SummaryCards from "../components/SummaryCards";
 import RecentServices from "../components/RecentServices";
-import { getDashboard } from "../services/dashboardService";
 import ExpenseChart from "../components/ExpenseChart";
+import { getDashboard } from "../services/dashboardService";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
   const [dashboard, setDashboard] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -25,40 +28,89 @@ function Dashboard() {
     fetchDashboard();
   }, []);
 
-  if (loading) {
+  if(loading) {
     return (
       <>
-        <Navbar />
-        <div className="text-center p-10 text-xl">
-          Loading Dashboard...
-        </div>
+      <Navbar />
+      <div className="text-center mt-20 text-xl">
+        Loading Dashboard...
+      </div>
+
       </>
     );
   }
 
   return (
     <>
-      <Navbar />
+    <Navbar />
+    <div className="max-w-7xl mx-autop p-6">
+      <h1 className="text-4xl font-bold mb-8">
+        Dashboard
+      </h1>
 
-      <div className="max-w-7xl mx-auto p-6">
-        <h1 className="text-4xl font-bold mb-8">
-          Dashboard
-        </h1>
+      <SummaryCards
+      totalVehicles={dashboard.totalVehicles}
+      totalServices={dashboard.totalServices}
+      totalCost={dashboard.totalCost}
+      />
 
-        <SummaryCards
-          totalVehicles={dashboard.totalVehicles}
-          totalServices={dashboard.totalServices}
-          totalCost={dashboard.totalCost}
-        />
-
-        <div className="mb-8">
-  <ExpenseChart />
-</div>
-
-        <RecentServices
-          services={dashboard.recentServices}
-        />
+      <div className="my-8">
+        <ExpenseChart />
       </div>
+
+      { /* My Vehicles */ }
+
+      <div className="mb-10">
+        <h2 className="text-2xl font-bold mb-4">
+          🚗 My Vehicles
+        </h2>
+        {dashboard.vehicles.length === 0 ? (
+          <div className="bg-white rounded-xl shadow p-6">
+            No vehicles found.
+            </div>
+        ) : (
+          <div className="grid md:grid-cols-2 gap-6">
+            {dashboard.vehicles.map((vehicle) =>(
+              <div
+              key={vehicle._id}
+              className="bg-white shadow-lg rounded-xl p-6"
+              >
+                <h3 className="text-xl font-bold">
+                  {vehicle.brand} {vehicle.model}
+                </h3>
+
+                <p className="text-grey-500 mt-2">
+                  {vehicle.registrationNumber}
+                </p>
+                <div className="flex gap-3 mt-5">
+                  <button
+                  onClick={() =>
+                    navigate(`/vehicles/${vehicle._id}`)
+                  }
+                  className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded"
+                  >
+                    View Details
+                  </button>
+
+                  <button
+                  onClick={() =>
+                    navigate(`/edit-vehicle/${vehicle._id}`)
+                  }
+                  className="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded"
+                  >
+                    Edit
+                  </button>
+                  </div>
+                  </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <RecentServices
+      services={dashboard.recentServices}
+      />
+    </div>
     </>
   );
 }
